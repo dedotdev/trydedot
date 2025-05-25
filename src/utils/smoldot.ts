f5376f09157dd5695b694ebfbc07d86af3252ffa
Thang X. Vu <thang@dedot.dev>
2025-05-25 12:13:17 +0700

    new initialize smoldot worker

--- src/utils/smoldot.ts
+++ src/utils/smoldot.ts
@@ -1,9 +1,13 @@
 import { Chain, Client } from 'dedot/smoldot';
 import { startWithWorker } from 'dedot/smoldot/with-worker';
-import SmoldotWorker from 'dedot/smoldot/worker?worker';
 
 export const newSmoldotChain = async (chainSpec: string): Promise<Chain> => {
-  const client: Client = startWithWorker(new SmoldotWorker());
+  const SmoldotWorker = new Worker(
+    new URL('dedot/smoldot/worker', import.meta.url), // --
+    { type: 'module' },
+  );
+
+  const client: Client = startWithWorker(SmoldotWorker);
 
   // TODO terminate on disconnect
   return client.addChain({ chainSpec });
